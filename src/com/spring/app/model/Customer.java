31a85c9e4b6b765cb97e75f857dacb44d9e0d59e
nagamandava <mandava.rky@gmail.com>
2024-02-24 10:04:31 -0500

    I added changes fro model class

---
+++ src/com/spring/app/model/Customer.java
@@ -0,0 +1,20 @@
+package com.spring.app.model;
+
+public class Customer {
+	private Long Id;
+	private String Name;
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	
+
+}
