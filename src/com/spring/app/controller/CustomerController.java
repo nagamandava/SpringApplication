d514b2b3285dee0e7715e49a44c6c9a6996915c2
nagamandava <mandava.rky@gmail.com>
2024-02-24 10:33:34 -0500

    changes made for controller

--- src/com/spring/app/controller/CustomerController.java
+++ src/com/spring/app/controller/CustomerController.java
@@ -2,4 +2,9 @@ package com.spring.app.controller;
 
 public class CustomerController {
 
+	private void psvm() {
+		// TODO Auto-generated method stub
+		System.out.println("newly added changes");
+
+	}
 }
