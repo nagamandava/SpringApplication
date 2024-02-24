6ecf3230fe98eb59cfe72984906b2f537446e00c
nagamandava <mandava.rky@gmail.com>
2024-02-24 09:59:18 -0500

    first commit

---
+++ src/com/spring/app/TestSpringClient.java
@@ -0,0 +1,11 @@
+package com.spring.app;
+
+public class TestSpringClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing springappclient completed");
+
+	}
+
+}
